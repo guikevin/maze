80aa4c4aacebf9783a92fc6ad379622402cc7e9d
guikevin <guikevin23@gmail.com>
2021-10-28 20:45:55 +0800

    The Maze Project

---
+++ The Maze/Assets/Script/pLayerController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pLayerController : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(movement * speed);
+    }
+}
